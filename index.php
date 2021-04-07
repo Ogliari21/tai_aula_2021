112bc773b3990c39d2a57660a8bf21b5f2860901
Leonardo Ogliari <leonardo.o27@aluno.ifsc.edu.br>
2021-04-07 08:45:46 -0300

    0704

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h4>Telas<h4>
+<a href = "./screens/usuarioForm.php">Usuário</a>
+    
+</body>
+</html>
---
+++ screens/usuarioList.php
@@ -0,0 +1,48 @@
+<?php 
+
+include '../database/bd.php';
+
+$objBD = new bd();
+
+//var_dump($objBD->connection());
+
+    $result = $objBD->select();
+    echo 'Registro inserido com sucesso!';
+
+?>
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<meta charset = "UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Listagem de Usuários</h4>
+    <a href = '../index.php'>Voltar</a>
+    <a href = './usuarioForm.php'>Cadastrar</a>
+
+    <table>
+    <th>ID</th>
+    <th>Nome</th>
+    <th>Telefone</th>
+    <th>CPF</th>
+    <th>Ação</th>
+    <?php
+    foreach($result as $item){
+        $item = (object) $item;
+        echo "
+        <tr>
+        <td>".$item->id."</td>
+        <td>".$item->nome."</td>
+        <td>".$item->telefone."</td>
+        <td>".$item->cpf."</td>
+        <td><a href = 'usuarioForm.php?id=". $item->id."'>Editar</a></td>
+        </tr>";
+    }
+    ?>
+    </table>
+
+</body>
+</html>
